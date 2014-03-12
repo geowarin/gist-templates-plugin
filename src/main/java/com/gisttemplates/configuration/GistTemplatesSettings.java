6f895c2f5849b0db2024192b859cf94e5ec1e10f
geowarin <geowarin@gmail.com>
2014-03-12 21:52:01 +0100

    Added editor config file

--- src/main/java/com/gisttemplates/configuration/GistTemplatesSettings.java
+++ src/main/java/com/gisttemplates/configuration/GistTemplatesSettings.java
@@ -1,6 +1,9 @@
 package com.gisttemplates.configuration;
 
-import com.intellij.openapi.components.*;
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
 import com.intellij.openapi.diagnostic.Logger;
 import org.jdom.Element;
 import org.jetbrains.annotations.Nullable;
